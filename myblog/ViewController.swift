1784a93ce9c9a9a7484334be816df4b73e7a9d63
王海峰 <wanghaifeng@calfmobile.com>
2016-10-18 16:22:17 +0800

    Initial Commit

---
+++ myblog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myblog
+//
+//  Created by chinaskin on 16/10/18.
+//  Copyright © 2016年 wanghaifeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
